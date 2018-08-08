08bc7b7f103edcc0833ab961f6f55314a8f15e5c
TechnicsUser <stephensmith99972@gmail.com>
2018-08-08 23:05:43 +0100

    notifications, create, edit, delete

--- WebApplication9/Models/Notification.cs
+++ WebApplication9/Models/Notification.cs
@@ -11,12 +11,15 @@ namespace WebApplication9.Models {
             }
 
         public class Notification {
-            public int NotificationId { get; set; }
-            public string Title { get; set; }
-            public NotificationType NotificationType { get; set; }
-            public string Controller { get; set; }
-            public string Action { get; set; }
-            }
+        public int NotificationId { get; set; }
+        public string Title { get; set; }
+        public NotificationType NotificationType { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string UserId { get; set; }
+        public bool IsDismissed { get; set; }
+
+        }
 
         }
    
