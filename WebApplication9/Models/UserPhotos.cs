6cefec18ee5c35fd4219d8cc0c75f52b7bae73f6
TechnicsUser <stephensmith99972@gmail.com>
2018-08-10 20:01:13 +0100

    added coral view

---
+++ WebApplication9/Models/UserPhotos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication9.Models {
+    public class UserPhotos {
+        public enum PhotoOf {
+            Coral,
+            User,
+            Tank,
+            Equipment,
+            Other
+            }
+        public enum Lighting {
+            T5,
+            T8,
+            Led,
+            Mh,
+            T5LedCombo,
+            Other
+            }
+        [Key]
+        int PhotoId { get; set; }
+        byte[] Photo { get; set; }
+
+        public PhotoOf Type { get; set; }
+        public Lighting LightingType { get; set; }
+        public string UserId { get; set; }
+        public string CreatedOn { get; set; }
+        public string RemovedOn { get; set; }
+        public string LastAccessed { get; set; }
+
+        public string CameraType { get; set; }
+
+
+        public int Views { get; set; }
+        public string Format { get; set; }
+
+
+
+
+
+        }
+    }
