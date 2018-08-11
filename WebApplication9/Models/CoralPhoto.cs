b9c9308f755d45bb5b6595a1826d16e9ff09ac10
TechnicsUser <stephensmith99972@gmail.com>
2018-08-11 15:57:26 +0100

    multi photo, coral view

--- WebApplication9/Models/CoralPhoto.cs
+++ WebApplication9/Models/CoralPhoto.cs
@@ -5,14 +5,24 @@ using System.Linq;
 using System.Web;
 
 namespace WebApplication9.Models {
-    public class CoralPhotos {
-       [Key ]
+    public class CoralPhoto {
 
+
+        [Key ]
         public int CoralPhotoId { get; set; }
+        public byte[] Photo { get; set; }
+
         public int CoralId { get; set; }
         public string UserId { get; set; }
+
+        public string CommentId { get; set; }
+        public int Likes { get; set; }
+        public int DisLikes { get; set; }
+        public int Views { get; set; }
+
         public string CreatedOn { get; set; }
         public string RemovedOn { get; set; }
+
         public bool Removed { get; set; }
 
 
