7b8e218cb52e4aa6af1bae4b92eeeeaac12957ad
TechnicsUser <stephensmith99972@gmail.com>
2018-08-11 11:22:41 +0100

    coralPhoto model

---
+++ WebApplication9/Models/CoralPhotos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication9.Models {
+    public class CoralPhotos {
+       [Key ]
+
+        public int CoralPhotoId { get; set; }
+        public int CoralId { get; set; }
+        public string UserId { get; set; }
+        public string CreatedOn { get; set; }
+        public string RemovedOn { get; set; }
+        public bool Removed { get; set; }
+
+
+
+        }
+    }
