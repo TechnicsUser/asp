dbb47a87552121b7aa2682de9380ddf9953fd79a
TechnicsUser <stephensmith99972@gmail.com>
2018-09-03 14:38:11 +0100

    fixed change password email

--- WebApplication9/Controllers/AccountController.cs
+++ WebApplication9/Controllers/AccountController.cs
@@ -218,7 +218,7 @@ namespace WebApplication9.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = await UserManager.FindByNameAsync(model.Email);
+                var user = await UserManager.FindByEmailAsync(model.Email);
                 if (user == null || !(await UserManager.IsEmailConfirmedAsync(user.Id)))
                 {
                     // Don't reveal that the user does not exist or is not confirmed
@@ -264,7 +264,7 @@ namespace WebApplication9.Controllers
             {
                 return View(model);
             }
-            var user = await UserManager.FindByNameAsync(model.Email);
+            var user = await UserManager.FindByEmailAsync(model.Email);
             if (user == null)
             {
                 // Don't reveal that the user does not exist
