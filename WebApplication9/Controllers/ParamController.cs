c8f11fd1252cf9e08c12f30c9203a7bed8cdc85f
TechnicsUser <stephensmith99972@gmail.com>
2019-03-27 14:58:50 +0000

    Added Param

---
+++ WebApplication9/Controllers/ParamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication9.Controllers
+{
+    public class ParamController : Controller
+    {
+        // GET: Param
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
